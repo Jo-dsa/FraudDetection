488f4ebd6ea8fb52a87e76ebf111d5e63a1c4c8a
Jo.dsa <61558609+Jo-dsa@users.noreply.github.com>
2020-05-18 16:37:09 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,45 +1,47 @@
-from utils.Model import Framework
-from utils.Preprocess import Preprocessing
-
 import pandas as pd
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from xgboost import XGBClassifier
 
-if __name__ == '__main__':
-	"""
-	Main file to train and/or predict
-	"""
-	
-	# Parameters
-	RANDOM_STATE = 42
-	METHOD = 'undersampling'
-	FILE = './data/creditcard.csv'
-	SAVED_DIR = './saved_models'
+from src.model import Framework
+from src.preprocess import Preprocessing
 
-	# Read data
-	df = pd.read_csv(FILE)
-	# Clean data
-	Processing = Preprocessing().fit(df, columns_name=['Time','Amount'])
-	# Get sample using oversampling or undersampling
-	Xtrain, Xtest, ytrain, ytest =  Processing.get_sample(method=METHOD, t_size=.3, random_state=RANDOM_STATE )
+if __name__ == "__main__":
+    """
+    Main file to train and/or predict
+    """
 
-	# Train & save models	
-	my_models = {
-		'Logistic_regression':LogisticRegression(random_state = RANDOM_STATE),
-		'Naive_Bayes':GaussianNB(),
-		'XGBoost':XGBClassifier(),
-	}
-	ensemble_learners = Framework(models=my_models).fit(Xtrain, ytrain, SAVED_DIR)
-	
-	# Get performance
-	s = ensemble_learners.scores(Xtest, ytest)
-	print(pd.DataFrame({'model_name':s[0],'AUC':s[1]}))
+    # Parameters
+    RANDOM_STATE = 42
+    METHOD = "undersampling"
+    FILE = "./data/creditcard.csv"
+    SAVED_DIR = "./pretrained"
 
-	# predict
-	yhat = Framework.predict(Xtest, "./saved_models/Logistic_regression.pkl")
+    # 1) Training
 
+    # Load & process data
+    df = pd.read_csv(FILE)
+    Processing = Preprocessing().fit(df, columns_name=["Time", "Amount"])
 
+    # Apply sampling
+    Xtrain, Xtest, ytrain, ytest = Processing.get_sample(
+        method=METHOD, t_size=0.3, random_state=RANDOM_STATE
+    )
 
+    # Train & save models
+    my_models = {
+        "Logistic_regression": LogisticRegression(random_state=RANDOM_STATE),
+        "Naive_Bayes": GaussianNB(),
+        "XGBoost": XGBClassifier(),
+    }
+    learners = Framework(models=my_models).fit(Xtrain, ytrain, SAVED_DIR)
 
+    # Get performance score
+    scores = learners.get_scores(Xtest, ytest)
+    print(pd.DataFrame({"model_name": scores[0], "AUC": scores[1]}))
 
+    # 2) Inference
+
+    # Predict
+    # yhat = Framework.predict(Xtest, "./pretrained/Logistic_regression.pkl")
